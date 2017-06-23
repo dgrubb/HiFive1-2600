3a97ca37be32acafbb0902503996803e1c10fc97
dgrubb <davidanthonygrubb@gmail.com>
2017-06-23 13:55:41 -0500

    Update documentation
    1) Add extra TIA notes.
    2) Add something to README.md.

--- Atari-TIA.h
+++ Atari-TIA.h
@@ -4,13 +4,14 @@
  * Date: 06/23/17
  *
  * Implements the Atari 2600's custom Television Interface Adapter chip. This
- * provides audio and graphics drawing via analog beam chasig methods (as 
+ * provides audio and graphics drawing via analog beam chasing methods (as 
  * opposed to a stored framebuffer). The TIA also provides features for
  * providing simple game objects such as playfield, players and missiles.
  *
  * References:
  *
  * Stella Programmer's Guide - http://atarihq.com/danb/files/stella.pdf
+ * Chip pinout - http://atarihq.com/danb/tia.shtml
  */
 
 #ifndef _TIA_H
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # HiFive1-2600
+
+This may end up being a little too ambituous, but let's give it a shot anyway.
+
+[Hold onto your butts ...](https://www.youtube.com/watch?v=HKK4KmDlj8U)
+
