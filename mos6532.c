912fbcdabef91be1c899c61f121f131bb5d25129
dgrubb <davidanthonygrubb@gmail.com>
2017-06-26 15:49:53 -0500

    Add commentary to RIOT chip
    1) Add file headers for MOS6532 emulation.

---
+++ mos6532.c
@@ -0,0 +1,8 @@
+/*
+ * File: mos6532.c
+ * Author: dgrubb
+ * Date: 06/26/2017
+ *
+ * Provides implementation of MOS 6532 RAM-I/O-Timer (RIOT) chip. This handles
+ * both 128 bytes of RAM storage and digital I/O.
+ */
---
+++ mos6532.h
@@ -0,0 +1,14 @@
+/*
+ * File: mos6532.h
+ * Author: dgrubb
+ * Date: 06/26/2017
+ *
+ * Provides implementation of MOS 6532 RAM-I/O-Timer (RIOT) chip. This handles
+ * both 128 bytes of RAM storage and digital I/O.
+ */
+
+#ifndef _MOS6532_H
+#define _MOS6532_H
+
+#endif /* _MOS6532_H */
+
