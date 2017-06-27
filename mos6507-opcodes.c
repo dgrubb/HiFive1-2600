19fc393354cc4a2cdb29b18ef628d1fc32c27255
dgrubb <davidanthonygrubb@gmail.com>
2017-06-27 08:56:25 -0500

    Remove defunct code
    1) Remove references to cycle time from insutrction definitions. This shall be handled internally by the implementation function for each opcode instead.

--- mos6507-opcodes.c
+++ mos6507-opcodes.c
@@ -21,7 +21,6 @@ instruction_t ISA_table[ISA_LENGTH];
 void opcode_execute(char opcode)
 {
     ISA_table[opcode].opcode(
-        ISA_table[opcode].cycles,
         ISA_table[opcode].addressing_mode
     );
 }
@@ -36,7 +35,6 @@ void opcode_populate_ISA_table()
     /* Initially, fill the entire table with ILL */
     for (i=0; i<ISA_LENGTH; i++) {
         ISA_table[i].opcode = opcode_ILL;
-        ISA_table[i].cycles = 1;
         ISA_table[i].addressing_mode = OPCODE_ADDRESSING_MODE_IMPLIED;
     }
 
@@ -47,77 +45,62 @@ void opcode_populate_ISA_table()
 
     /* 0x00: BRK, Implied*/
     ISA_table[0x00].opcode = opcode_BRK;
-    ISA_table[0x00].cycles = 7;
     ISA_table[0x00].addressing_mode = OPCODE_ADDRESSING_MODE_IMPLIED;
 
     /* 0x01: ORA, X indexed indirect */
     ISA_table[0x01].opcode = opcode_ORA;
-    ISA_table[0x01].cycles = 6;
     ISA_table[0x01].addressing_mode = OPCODE_ADDRESSING_MODE_X_INDEXED_INDIRECT;
 
     /* 0x05: ORA, Zero-page */
     ISA_table[0x05].opcode = opcode_ORA;
-    ISA_table[0x05].cycles = 3;
     ISA_table[0x05].addressing_mode = OPCODE_ADDRESSING_MODE_ZERO_PAGE;
 
     /* 0x06: ASL, Zero-page */
     ISA_table[0x06].opcode = opcode_ASL;
-    ISA_table[0x06].cycles = 6;
     ISA_table[0x06].addressing_mode = OPCODE_ADDRESSING_MODE_ZERO_PAGE;
 
     /* 0x08: PHP, Implied */
     ISA_table[0x08].opcode = opcode_PHP;
-    ISA_table[0x08].cycles = 4;
     ISA_table[0x08].addressing_mode = OPCODE_ADDRESSING_MODE_IMPLIED;
 
     /* 0x09: ORA, Immediate */
     ISA_table[0x09].opcode = opcode_ORA;
-    ISA_table[0x09].cycles = 2;
     ISA_table[0x09].addressing_mode = OPCODE_ADDRESSING_MODE_IMMEDIATE;
 
     /* 0x0A: ASL, Accumulator */
     ISA_table[0x0A].opcode = opcode_ASL;
-    ISA_table[0x0A].cycles = 2;
     ISA_table[0x0A].addressing_mode = OPCODE_ADDRESSING_MODE_ACCUMULATOR;
 
     /* 0x0D: ORA, Absolute */
     ISA_table[0x0D].opcode = opcode_ORA;
-    ISA_table[0x0D].cycles = 4;
     ISA_table[0x0D].addressing_mode = OPCODE_ADDRESSING_MODE_ABSOLUTE;
 
     /* 0x0E: ASL, Absolute */
     ISA_table[0x0E].opcode = opcode_ASL;
-    ISA_table[0x0E].cycles = 6;
     ISA_table[0x0E].addressing_mode = OPCODE_ADDRESSING_MODE_ABSOLUTE;
 
     /* 0x10: BPL, Relative */
     ISA_table[0x10].opcode = opcode_BPL;
-    ISA_table[0x10].cycles = 2;
     ISA_table[0x10].addressing_mode = OPCODE_ADDRESSING_MODE_RELATIVE;
 
     /* 0x11: ORA, Indirect Y indexed */
     ISA_table[0x11].opcode = opcode_ORA;
-    ISA_table[0x11].cycles = 5;
     ISA_table[0x11].addressing_mode = OPCODE_ADDRESSING_MODE_INDIRECT_Y_INDEXED;
 
     /* 0x15: ORA, Zero-page X indexed */
     ISA_table[0x15].opcode = opcode_ORA;
-    ISA_table[0x15].cycles = 4;
     ISA_table[0x15].addressing_mode = OPCODE_ADDRESSING_MODE_ZERO_PAGE_X_INDEXED;
 
     /* 0x16: ASL, Zero-page X indexed */
     ISA_table[0x16].opcode = opcode_ASL;
-    ISA_table[0x16].cycles = 6;
     ISA_table[0x16].addressing_mode = OPCODE_ADDRESSING_MODE_ZERO_PAGE_X_INDEXED;
 
     /* 0x18: CLC, Implied */
     ISA_table[0x18].opcode = opcode_CLC;
-    ISA_table[0x18].cycles = 2;
     ISA_table[0x18].addressing_mode = OPCODE_ADDRESSING_MODE_IMPLIED;
 
     /* 0x19: ORA, Absolute Y indexed */
     ISA_table[0x19].opcode = opcode_ORA;
-    ISA_table[0x19].cycles = 4;
     ISA_table[0x19].addressing_mode = OPCODE_ADDRESSING_MODE_ABSOLUTE_Y_INDEXED;
 }
 
@@ -125,32 +108,32 @@ void opcode_populate_ISA_table()
  * Instruction set implementation
  *****************************************************************************/
 
-void opcode_ILL(int cycles, addressing_mode_t address_mode)
+void opcode_ILL(addressing_mode_t address_mode)
 {
     
 }
 
-void opcode_ASL(int cycle, addressing_mode_t address_mode)
+void opcode_ASL(addressing_mode_t address_mode)
 {
 }
 
-void opcode_BPL(int cycle, addressing_mode_t address_mode)
+void opcode_BPL(addressing_mode_t address_mode)
 {
 }
 
-void opcode_BRK(int cycles, addressing_mode_t address_mode)
+void opcode_BRK(addressing_mode_t address_mode)
 {
 }
 
-void opcode_CLC(int cycle, addressing_mode_t address_mode)
+void opcode_CLC(addressing_mode_t address_mode)
 {
 }
 
-void opcode_ORA(int cycle, addressing_mode_t address_mode)
+void opcode_ORA(addressing_mode_t address_mode)
 {
 }
 
-void opcode_PHP(int cycles, addressing_mode_t address_mode)
+void opcode_PHP(addressing_mode_t address_mode)
 {
     mos6532_write(cpu.S, cpu.P);
 }
