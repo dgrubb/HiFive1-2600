a86731085a6e5dc967964cc69010da87f45e873f
dgrubb <davidanthonygrubb@gmail.com>
2017-06-28 21:26:47 -0500

    Add file skeletons
    1) Add file skeletons for memory map.

---
+++ Atari-memmap.c
@@ -0,0 +1,8 @@
+/*
+ * File: Atari-memmap.c
+ * Author: dgrubb
+ * Date: 06/28/2017
+ *
+ * Delegates access to memory mapped devices.
+ */
+
---
+++ Atari-memmap.h
@@ -0,0 +1,14 @@
+/*
+ * File: Atari-memmap.h
+ * Author: dgrubb
+ * Date: 06/28/2017
+ *
+ * Delegates access to memory mapped devices.
+ */
+
+#ifndef _MEMMAP_H
+#define _MEMMAP_H
+
+
+
+#endif /* _MEMMAP_H */
