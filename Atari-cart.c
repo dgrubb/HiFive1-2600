c2db8da814687f76a23ee8c1368c79c39c4c112c
dgrubb <davidanthonygrubb@gmail.com>
2017-07-07 13:38:55 -0500

    Add skeleton to files for carts.

---
+++ Atari-cart.c
@@ -0,0 +1,8 @@
+/*
+ * File: Atari-cart.c
+ * Author: dgrubb
+ * Date: 07/07/2017
+ *
+ * Mimics ROM space (that is, a game cartridge).
+ */
+
---
+++ Atari-cart.h
@@ -0,0 +1,14 @@
+/*
+ * File: Atari-cart.h
+ * Author: dgrubb
+ * Date: 07/07/2017
+ *
+ * Mimics ROM space (that is, a game cartridge).
+ */
+
+#ifndef _ATARI_CART_H
+#define _ATARI_CART_H
+
+#include <stdint.h>
+
+#endif /* _ATARI_CART_H */
