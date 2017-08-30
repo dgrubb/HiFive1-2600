e0035ed3aad574f71badd76e80450b6e82ab87cf
dgrubb <davidanthonygrubb@gmail.com>
2017-08-30 09:52:20 -0500

    Make the memory mapping a little more functional in style.

--- atari/Atari-memmap.c
+++ atari/Atari-memmap.c
@@ -11,6 +11,18 @@
 #include "../cpu/mos6507.h"
 #include "../memory/mos6532.h"
 
+void memmap_map_address(uint16_t *address)
+{
+    /* The 6507 is a variant of the 6502. It shares the 16-bit addressing
+     * system internally, but only connects to 13 external pins so the 
+     * largest address which can be selected is 0x1FFF. However, as the 
+     * processor can still handle full 16-bit addresses a programmer may have
+     * used a greater value with the expectation those bits would be disregarded
+     * and mirroring would take place.
+     */
+    *address = (*address & 0x1FFF);
+}
+
 /* These read/write functions essentially replicate the 
  * chip-select pins on the TIA and RIOT by delegating 
  * memory access requests to their respective locations.
@@ -22,6 +34,7 @@ void memmap_write()
     uint8_t data;
     mos6507_get_data_bus(&data);
     mos6507_get_address_bus(&address);
+    memmap_map_address(&address);
 
     /* Access particular device */
     if (address >= MEMMAP_TIA_START && address <= MEMMAP_TIA_END) {
@@ -38,15 +51,7 @@ void memmap_read(uint8_t *data)
     /* Fetch address from CPU */
     uint16_t address;
     mos6507_get_address_bus(&address);
-
-    /* The 6507 is a variant of the 6502. It shares the 16-bit addressing
-     * system internally, but only connects to 13 external pins so the 
-     * largest address which can be selected is 0x1FFF. However, as the 
-     * processor can still handle full 16-bit addresses a programmer may have
-     * used a greater value with the expectation those bits would be disregarded
-     * and mirroring would take place.
-     */
-    address = (address & 0x1FFF);
+    memmap_map_address(&address);
 
     /* Access particular device */
     if (address >= MEMMAP_TIA_START && address <= MEMMAP_TIA_END) {
