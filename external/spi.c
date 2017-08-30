037f1d20924beb0a6cdf8f7d1715b47616db28ac
dgrubb <davidanthonygrubb@gmail.com>
2017-08-30 11:52:49 -0500

    Add a model for handling I/O over the SPI interface.

---
+++ external/spi.c
@@ -0,0 +1,8 @@
+/*
+ * File: spi.c
+ * Author: dgrubb
+ * Date: 08/30/2017
+ *
+ * Handle I/O with external peripherals on the SPI interface.
+ */
+
---
+++ external/spi.h
@@ -0,0 +1,15 @@
+/*
+ * File: spi.h
+ * Author: dgrubb
+ * Date: 08/30/2017
+ *
+ * Handle I/O with external peripherals on the SPI interface.
+ */
+
+#ifndef _SPI_H
+#define _SPI_H
+
+
+
+#endif /* _SPI_H */
+
