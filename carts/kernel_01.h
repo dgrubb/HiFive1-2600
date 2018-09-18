a979ab8973eb4af949249255b9d675e51a780e2e
David Grubb <davidanthonygrubb@gmail.com>
2018-09-18 12:48:29 -0500

    Add some test carts.

---
+++ carts/kernel_01.h
@@ -0,0 +1,9 @@
+#ifndef _KERNEL_01_H
+#define _KERNEL_01_H
+
+#include <stdint.h>
+
+extern uint8_t kernel_01[4096];
+extern int kernel_01_len;
+
+#endif // _KERNEL_01_H
---
+++ carts/kernel_11.h
@@ -0,0 +1,9 @@
+#ifndef _KERNEL_11_H
+#define _KERNEL_11_H
+
+#include <stdint.h>
+
+extern uint8_t kernel_11[4096];
+extern int kernel_11_len;
+
+#endif // _KERNEL_11_H
